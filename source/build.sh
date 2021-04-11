2766115e13c69015edea3ed45b80842c4a203ae1
1234ab <nevenincs007@gmail.com>
2021-04-11 21:55:15 +0200

    minor buildscript improvements

--- source/build.sh
+++ source/build.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 #this is so that you can run the script from other directories too
-cd $(dirname $0)/build
+cd $(dirname $0)
+mkdir build
+cd build
 echo "starting the building for Linux in $PWD"
 cmake ..
 make -j4
--- source/build_windows.sh
+++ source/build_windows.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 #this is so that you can run the script from other directories too
-cd $(dirname $0)/build
+cd $(dirname $0)
+mkdir build
+cd build
 echo "starting the building for Windows in $PWD"
+cmake -DCMAKE_TOOLCHAIN_FILE=../mingw-w64-x86_64.cmake ..
+make -j4
+#somehow the toolchain's last line only works if I run it twice
 cmake -DCMAKE_TOOLCHAIN_FILE=../mingw-w64-x86_64.cmake ..
 make -j4
